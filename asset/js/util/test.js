7ae4b52b3f928bb27cabff599a0cf65d748c26b8
Lee <lihaiboloveyou@live.com>
2016-11-30 16:48:16 +0800

    测试重力感应

---
+++ asset/js/util/test.js
@@ -0,0 +1,16 @@
+//var worker = new Worker("worker.js");
+//worker.onmessage = function (eve) {
+//    console.log(eve.data);
+//}
+console.log(window.DeviceOrientationEvent);
+
+$(window).on('deviceorientation', function (e) {
+    console.log('ppp')
+});
+
+window.addEventListener('devicemotion', function (e) {
+    var acceleration = event.accelerationIncludingGravity;
+    var x = acceleration.x;
+    var y = acceleration.y;
+    console.log(acceleration)
+})
---
+++ asset/js/util/worker.js
@@ -0,0 +1,5 @@
+var i = 0;
+setInterval(function () {
+    //postMessage(i++);
+    console.log(i++)
+},100);
