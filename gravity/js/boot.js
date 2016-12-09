d0093d3c30de0ea13d43c3dad3e1305945791fa7
Lee <lihaiboloveyou@live.com>
2016-12-09 14:33:44 +0800

    调整目录结构

--- gravity/js/boot.js
+++ gravity/js/boot.js
@@ -10,7 +10,7 @@ var bootState = function (game) {
     }
 
     this.preload = function () {
-        game.load.image('preload','../../img/preloader.gif');
+        game.load.image('preload','img/preloader.gif');
     }
     this.create = function () {
         game.state.start('load');
---
+++ gravity/js/load.js
@@ -0,0 +1,21 @@
+var loadState = function (game) {
+
+    this.preload = function () {
+        var preload = game.add.sprite(game.width/2,game.height/2,'preload');
+        preload.anchor.setTo(0.5);
+        game.load.setPreloadSprite(preload);
+        game.load.image('bg','img/IMG_1138.JPG');
+        game.load.image('plane','img/thrust_ship.png');
+        game.load.image('chunk','img/chunk.png');
+        game.load.image('e1','img/space-baddie.png');
+        game.load.image('e2','img/space-baddie-purple.png');
+        game.load.spritesheet('boom','img/boom32wh12.png',32,32);
+        game.load.audio('hit','music/hit.wav');
+        game.load.image('boos','img/wasp.png');
+        game.load.image('life','img/life.png');
+    }
+    
+    this.create = function () {
+        game.state.start('play');
+    }
+}
