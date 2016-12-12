c541638155889511bf163e6719710943f513cb85
lihai <lihaiboloveyou@live.com>
2016-12-12 21:26:34 +0800

    完成方块落地时绘制方块

--- gameTetris/js/play.js
+++ gameTetris/js/play.js
@@ -1,12 +1,11 @@
 var playState = function (game) {
-    var d=10,r=100;
-    var drawMap,chick,createBox;
-    var tileMap,layer1,mybox;
-    var stats,cursors;
+    var d = 10, r = 100;
+    var drawMap, chick, createBox;
+    var tileMap, layer1, mybox;
+    var stats, cursors;
+    var x = d * 20, y = r + d;
     this.init = function () {
-
         createBox = new CreateBox();
-
 
         stats = new Stats();
         stats.setMode(0); // 0: fps, 1: ms
@@ -18,30 +17,31 @@ var playState = function (game) {
     this.create = function () {
         cursors = this.input.keyboard.createCursorKeys();
         tileMap = game.add.tilemap();
-        tileMap.addTilesetImage('tileset','tile',d,d);
-        layer1 = tileMap.create('leave1',game.width/d,game.height/d,d,d);
+        tileMap.addTilesetImage('tileset', 'tile', d, d);
+        layer1 = tileMap.create('leave1', game.width / d, game.height / d, d, d);
         layer1.scrollFactorX = 0.5;
         layer1.scrollFactorY = 0.5;
         layer1.resizeWorld();
 
-        drawMap = new DrawMap(tileMap,layer1);
-        chick = new Chick(tileMap,layer1);
-        drawMap.drawGrid(d,r);
+        drawMap = new DrawMap(tileMap, layer1, d);
+        chick = new Chick(tileMap, layer1);
+        drawMap.drawGrid(r);
 
-        drawMap.drawBound(r,d,3);
-        tileMap.putTile(4,50,30,layer1);
-        mybox = createBox.createMyBox(4,d*20,r+d);
+        drawMap.drawBound(r, 3);
+        tileMap.putTile(4, 50, 30, layer1);
+        mybox = createBox.createMyBox(0, x, y);
         game.input.onDown.add(function () {
             //mybox.angle+=90;
         });
         setInterval(function () {
-            if (chick.chickMove(mybox,d,40)){
-                mybox.y+=10;
-            }else{
-                mybox.x = d*20;
-                mybox.y = r+d;
+            if (chick.chickMove(mybox, d, 40)) {
+                mybox.y += 10;
+            } else {
+                drawMap.drawBox(mybox);
+                mybox.kill();
+                mybox = createBox.createMyBox(Math.floor(Math.random() * 4), x, y);
             }
-        },500);
+        }, 500);
 
         //键盘监听
         document.onkeydown = function (event) {
@@ -50,37 +50,36 @@ var playState = function (game) {
                 //if (chick.chickAngle(mybox,d,tileMap,layer1)){
                 //    mybox.angle+=90;
                 //}
-                chick.chickAngle(mybox,d)
+                chick.chickAngle(mybox, d)
             }
             if (e && e.keyCode == 40) { // 按 down
-                if (chick.chickMove(mybox,d,40)){
-                    mybox.y+=10;
+                if (chick.chickMove(mybox, d, 40)) {
+                    mybox.y += 10;
                 }
             }
             if (e && e.keyCode == 37) { // 按 left
-                if (chick.chickMove(mybox,d,37)){
-                    mybox.x-=d;
+                if (chick.chickMove(mybox, d, 37)) {
+                    mybox.x -= d;
                 }
             }
 
             if (e && e.keyCode == 39) { // 按 right
-                if (chick.chickMove(mybox,d,39)){
-                    mybox.x+=d
+                if (chick.chickMove(mybox, d, 39)) {
+                    mybox.x += d
                 }
             }
-            if(e && e.keyCode == 65){
-                mybox.y-=10
+            if (e && e.keyCode == 65) {
+                mybox.y -= 10
             }
         }
     }
     this.update = function () {
 
 
-
         stats.update();
     }
 
-    function hasTile(x,y,layer){
-        return tileMap.hasTile(x,y,layer);
+    function hasTile(x, y, layer) {
+        return tileMap.hasTile(x, y, layer);
     }
 }
--- gameTetris/js/util/drawMap.js
+++ gameTetris/js/util/drawMap.js
@@ -1,38 +1,125 @@
-class DrawMap{
-    constructor(map,layer){
+class DrawMap {
+    constructor(map, layer, d) {
         this.map = map;
         this.layer = layer;
+        this.d = d;
     }
+
     //画网格
-    drawGrid(d,r){
-            var graphics = game.add.graphics(0, 0);
-            graphics.lineStyle(1, '0x00ff00', 0.3);
-            for (var i = r; i <= game.width-r; i += d) {
-                graphics.moveTo(i,r);
-                graphics.lineTo(i, game.height-r);
-            }
-            for (var i = r;i<=game.height-r;i+=d){
-                graphics.moveTo(r,i);
-                graphics.lineTo(game.width-r,i);
-            }
+    drawGrid(r) {
+        var graphics = game.add.graphics(0, 0);
+        graphics.lineStyle(1, '0x00ff00', 0.3);
+        for (var i = r; i <= game.width - r; i += this.d) {
+            graphics.moveTo(i, r);
+            graphics.lineTo(i, game.height - r);
+        }
+        for (var i = r; i <= game.height - r; i += this.d) {
+            graphics.moveTo(r, i);
+            graphics.lineTo(game.width - r, i);
+        }
     }
 
     //画边界
-    drawBound(r,d,c){
-        var b = r/d;
-        for (var i = b;i<game.height/d-b;i++){
-            this.map.putTile(c,(b-1),i,this.layer);
+    drawBound(r, c) {
+        var b = r / this.d;
+        for (var i = b; i < game.height / this.d - b; i++) {
+            this.map.putTile(c, (b - 1), i, this.layer);
         }
-        for (var i = b;i<game.height/d-b+1;i++){
-            this.map.putTile(c,game.width/d-b,i,this.layer);
+        for (var i = b; i < game.height / this.d - b + 1; i++) {
+            this.map.putTile(c, game.width / this.d - b, i, this.layer);
         }
-        for(var i=b-1;i<game.width/d-b;i++ ){
-            this.map.putTile(c,i,game.height/d-b,this.layer);
+        for (var i = b - 1; i < game.width / this.d - b; i++) {
+            this.map.putTile(c, i, game.height / this.d - b, this.layer);
         }
     }
 
     //画方块
-    drawBox(box){
+    drawBox(box) {
+        var x = box.x / this.d;
+        var y = box.y / this.d;
+        if (box.key == 'l') {
+            switch (box.angle) {
+                case 0:
+                    this.map.putTile(0, x - 1, y, this.layer);
+                    this.map.putTile(0, x - 1, y - 1, this.layer);
+                    this.map.putTile(0, x - 1, y + 1, this.layer);
+                    this.map.putTile(0, x, y + 1, this.layer);
+                    break;
+                case 90:
+                    this.map.putTile(0, x, y - 1, this.layer);
+                    this.map.putTile(0, x - 1, y - 1, this.layer);
+                    this.map.putTile(0, x - 2, y - 1, this.layer);
+                    this.map.putTile(0, x - 2, y, this.layer);
+                    break;
+                case -180:
+                    this.map.putTile(0, x, y, this.layer);
+                    this.map.putTile(0, x, y - 1, this.layer);
+                    this.map.putTile(0, x, y - 2, this.layer);
+                    this.map.putTile(0, x - 1, y - 2, this.layer);
+                    break;
+                case -90:
+                    this.map.putTile(0, x, y, this.layer);
+                    this.map.putTile(0, x - 1, y, this.layer);
+                    this.map.putTile(0, x + 1, y, this.layer);
+                    this.map.putTile(0, x + 1, y - 1, this.layer);
+                    break;
 
+            }
+        }
+        if (box.key == 'o') {
+            switch (box.angle) {
+                case 0:
+                    this.map.putTile(1, x, y, this.layer);
+                    this.map.putTile(1, x - 1, y, this.layer);
+                    this.map.putTile(1, x, y - 1, this.layer);
+                    this.map.putTile(1, x - 1, y - 1, this.layer);
+                    break;
+            }
+        }
+        if (box.key == 'i') {
+            switch (box.angle) {
+                case 0:
+                    this.map.putTile(4, x, y, this.layer);
+                    this.map.putTile(4, x, y+1,this.layer);
+                    this.map.putTile(4, x, y-1, this.layer);
+                    this.map.putTile(4, x, y-2, this.layer);
+                    break;
+                case 90:
+                    this.map.putTile(4,x+1,y,this.layer);
+                    this.map.putTile(4,x,y,this.layer);
+                    this.map.putTile(4,x-1,y,this.layer);
+                    this.map.putTile(4,x-2,y,this.layer);
+                    break;
+            }
+        }
+        if(box.key == 't'){
+            switch (box.angle){
+                case 0:
+                    this.map.putTile(2,x,y,this.layer);
+                    this.map.putTile(2,x-1,y,this.layer);
+                    this.map.putTile(2,x+1,y,this.layer);
+                    this.map.putTile(2,x,y-1,this.layer);
+                    break;
+                case 90:
+                    this.map.putTile(2,x,y,this.layer);
+                    this.map.putTile(2,x-1,y,this.layer);
+                    this.map.putTile(2,x-1,y-1,this.layer);
+                    this.map.putTile(2,x-1,y+1,this.layer);
+                    break;
+                case -180:
+                    this.map.putTile(2,x-1,y,this.layer);
+                    this.map.putTile(2,x-1,y-1,this.layer);
+                    this.map.putTile(2,x-2,y-1,this.layer);
+                    this.map.putTile(2,x,y-1,this.layer);
+                    break;
+                case -90:
+                    this.map.putTile(2,x,y,this.layer);
+                    this.map.putTile(2,x,y-1,this.layer);
+                    this.map.putTile(2,x,y-2,this.layer);
+                    this.map.putTile(2,x-1,y-1,this.layer);
+                    break;
+
+            }
+        }
     }
 }
